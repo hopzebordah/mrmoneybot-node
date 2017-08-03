6fb684f9c8ec68f02b0b61e0f4171142c0c400cf
alex peters <aalcocerpeters@gmail.com>
2017-08-03 17:47:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ config.consumer_secret = process.env.consumer_secret
 config.access_token = process.env.access_token
 config.access_token_secret = process.env.access_token_secret
 
-config.bot_screen_name = 'mrmoneybot'
-config.bot_id = '832445967320358912'
+config.bot_screen_name = ''       //your bot's screen name (without the @, i.e. realDonaldTrump
+config.bot_id = ''                //your bot's user id (some string of numbers, i.e. '123123123123')
 
-config.owner_screen_name = 'aalcocerpeters'
-config.owner_id = '2178124183'
+config.owner_screen_name = ''     //the bot's owner's screen name 
+config.owner_id = ''              //the bot's owner's user id 
 
 module.exports = config
